bffd2f1aa1ae69c6765e77f4b488476aa1520dc6
Felipe Kaufmann <felipekaufmann@gmail.com>
2011-11-24 16:23:37 +0100

    dont depend on maintenance.txt being there

--- lib/mmode/cli.rb
+++ lib/mmode/cli.rb
@@ -5,12 +5,12 @@ module Mmode
 
     desc 'enable [USER]', 'Enable maintenance mode for given user'
     def enable(user)
-      `ssh scrcpt2.nine.ch 'touch /home/usr/screenconcept/maintenance.txt && sudo monit unmonitor -g thin_#{user}'`
+      `ssh scrcpt2.nine.ch 'touch /home/usr/screenconcept/maintenance.txt; sudo monit unmonitor -g thin_#{user}'`
     end
 
     desc 'disable [USER]', 'Disable maintenance mode for given user'
     def disable(user)
-      `ssh scrcpt2.nine.ch 'rm /home/usr/screenconcept/maintenance.txt && sudo monit monitor -g thin_#{user}'`
+      `ssh scrcpt2.nine.ch 'rm /home/usr/screenconcept/maintenance.txt; sudo monit monitor -g thin_#{user}'`
     end
 
   end
